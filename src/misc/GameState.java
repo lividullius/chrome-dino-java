886d44d1fbe5a85242a49f1c6767ce4ee53d82dc
lividullius <livia.dullius@edu.pucrs.br>
2024-11-24 10:40:18 -0300

    Update GameState.java

--- src/misc/GameState.java
+++ src/misc/GameState.java
@@ -3,5 +3,8 @@ package misc;
 public enum GameState {
 	
 	GAME_STATE_START, GAME_STATE_IN_PROGRESS, GAME_STATE_OVER, GAME_STATE_PAUSED, GAME_STATE_INTRO;
-	
+}
+
+public enum GamePhase {  //enumerador para representar as fases do jogo
+    PHASE_1, PHASE_2, PHASE_3;
 }
